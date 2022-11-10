1a3edd19c328ced4c736844afebeedaffcdaefb5
Mustafa Erdoğan <erdogann.mustafaaa@gmail.com>
2022-11-10 15:11:05 +0300

    error fixed

--- src/Hooks/useCollection.js
+++ src/Hooks/useCollection.js
@@ -4,7 +4,7 @@ import { collection,onSnapshot,query,where } from "firebase/firestore";
 import { useState,useEffect,useRef} from "react";
 
 export const useCollection = (col,_q)=>{
-    {/* Belgelere erişme işlemi. Yani firebase database de olan verileri ana sayfada görüntüleme.. */}
+    
     const [documents,setDocuments] = useState(null);
 
     const isQuery = useRef(_q).current;
